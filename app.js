39001e5f9f688a1358c5648bdc6225bbd54fc177
Def <deferenseo@gmail.com>
2023-03-21 21:57:13 +0300

    finished clock

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+const dayNode = document.querySelector('.day');
+const hoursNode = document.querySelector('.hours');
+const minutesNode = document.querySelector('.minutes');
+const meridiemNode = document.querySelector('.meridiem');
 
+function displayDate() {
+    const now = new Date();
 
-const now = new Date();
+    const date = {
+        weekday: now.toDateString().slice(0, 3),
+        hours: now.getHours(),
+        minutes: now.getMinutes(),
+        meridiem: this.hours > 0 && this.hours < 12 ? 'AM' : 'PM',
+    };
 
-const date = {
-    weekday: now.toDateString().slice(0, 3),
-    hours: now.getHours(),
-    minutes: now.getMinutes(),
-    meridiem: this.hours > 0 && this.hours < 12 ? 'AM' : 'PM',
-};
+    dayNode.innerHTML = (date.weekday).toUpperCase();
+    hoursNode.innerHTML = addZeroToNum(date.hours);
+    minutesNode.innerHTML = addZeroToNum(date.minutes);
+    meridiemNode.innerHTML = date.meridiem;
+}
 
-console.log(date);
+setInterval(displayDate, 1000);
 
-
+function addZeroToNum(num) {
+    if (num < 9) {
+        return '0' + num;
+    } else {
+        return num;
+    }
+}
